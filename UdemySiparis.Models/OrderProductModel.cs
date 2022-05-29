5867265985c42a89e230a518f8f82dc233aee8f5
Hakan GÜL <hakanngul@icloud.com>
2022-05-29 17:53:55 +0300

    order-product- ok

--- UdemySiparis.Models/OrderProductModel.cs
+++ UdemySiparis.Models/OrderProductModel.cs
@@ -1,6 +1,19 @@
-﻿namespace UdemySiparis.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UdemySiparis.Models
 {
     public class OrderProductModel
     {
+        [Key] public int Id { get; set; }
+        public string AppUserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double OrderPrice { get; set; }
+        public string OrderStatus { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string ReceiverName { get; set; }
+        [ForeignKey("AppUserId")] public AppUserModel AppUser { get; set; }
     }
 }
