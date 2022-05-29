6ff918904a4046c17a9a78d146406535ff5e9daf
Hakan GÜL <hakanngul@icloud.com>
2022-05-29 17:48:23 +0300

    cartmodel - ok

--- UdemySiparis.Models/CategoryModel.cs
+++ UdemySiparis.Models/CategoryModel.cs
@@ -1,6 +1,10 @@
-﻿namespace UdemySiparis.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemySiparis.Models
 {
     public class CategoryModel
     {
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; }
     }
 }
