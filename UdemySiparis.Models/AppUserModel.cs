21f8e26350a9172f4bada747f03535f6ad8c40da
Hakan GÜL <hakanngul@icloud.com>
2022-05-29 17:41:43 +0300

    AppUserModel - complated

--- UdemySiparis.Models/AppUserModel.cs
+++ UdemySiparis.Models/AppUserModel.cs
@@ -1,6 +1,16 @@
-﻿namespace UdemySiparis.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UdemySiparis.Models
 {
-    public class AppUserModel
+    public class AppUserModel : IdentityUser
     {
+        [Required] public string FullName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string MobilePhone { get; set; } = string.Empty;
+        [NotMapped] public string Role { get; set; } = string.Empty;
+
     }
 }
